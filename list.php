cea95ce14054bc7c6f841a911ae1c7e894303cd9
Danny van Kooten <dannyvankooten@gmail.com>
2015-04-25 22:42:24 +0200

    Calculate total per VAT number, add class to input field, stop at 100 rows.

--- list.php
+++ list.php
@@ -26,25 +26,71 @@ $response = $client->post('/invoices/filter/advanced.xml', [
 $invoices = $response->xml();
 echo count( $invoices ) . " invoices fetched." . PHP_EOL;
 
-echo "Generating HTML for Belastingdienst.nl site." . PHP_EOL;
+echo "Calculating totals per VAT number." . PHP_EOL;
 
-// loop through reverse charged invoices and generate correct HTML for Belastingdienst.nl
-$html = '';
-$index = 0;
+// calculate the totals for each VAT number
+$companies = array();
 foreach( $invoices as $invoice ) {
 
+	if( empty( $invoice->{"tax-number"} ) || empty( $invoice->country ) ) {
+		continue;
+	}
+
+	if( $invoice->country === 'GR' ) {
+		continue;
+	}
+
+	if( $invoice->currency !== 'EUR' ) {
+		$total = $invoice->{"total-price-incl-tax"} * ( USD_TO_EUR_EXCHANGE_RATE / 1.02 );
+	} else {
+		$total = $invoice->{"total-price-incl-tax"};
+	}
+
 	// we need the vat number without the country code
-	$vat_number = ( substr( $invoice->{"tax-number"}, 0,     2 ) === $invoice->country ) ? substr( $invoice->{"tax-number"}, 2 ) : $invoice->{"tax-number"};
+	$country = substr( $invoice->{"tax-number"}, 0, 2 );
+	$vat_number = (string) substr( $invoice->{"tax-number"}, 2 );
+
+	// add to total or create new entry
+	if( array_key_exists( $vat_number, $companies ) ) {
+		$companies[ $vat_number ]->total_services += floor( $total );
+	} else {
+		$company = new StdClass;
+		$company->total_services = floor( $total );
+		$company->country_code = $country;
+		$company->vat_number = $vat_number;
+		$companies[ $vat_number ] = $company;
+	}
+}
+
+
+// loop through reverse charged invoices and generate correct HTML for Belastingdienst.nl
+echo "Generating HTML for Belastingdienst.nl site." . PHP_EOL;
+
+$html = '';
+$index = 0;
+$total = 0;
+
+foreach( $companies as $company ) {
 
 	$row_html = generate_row_html( array(
 		'index' => $index++,
-		'vat_number' => $vat_number,
-		'country_code' => $invoice->country,
-		'total_services' => round( $invoice->{"total-price-incl-tax"} )
+		'vat_number' => $company->vat_number,
+		'country_code' => $company->country_code,
+		'total_services' => floor( $company->total_services )
 	) );
 
 	$html .= $row_html . PHP_EOL . PHP_EOL;
+
+	$total += floor( $company->total_services );
+
+	// quit at 100 rows
+	if( $index >= 100 ) {
+		echo "More than 100 company VAT numbers were found, but you can only submit up to a 100.... " . PHP_EOL;
+		break;
+	}
 }
+
+echo "Total reverse charged: " . $total . PHP_EOL;
 
 // write final html to a file: /build/icp-YEAR-QUARTER.html
 $filename = '/build/icp-'. date('Y') . '-' . ceil( ( ( date( 'm' ) - 3 ) / 3 ) ) . '.html';
