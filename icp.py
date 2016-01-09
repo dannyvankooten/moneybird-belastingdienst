e09aa7b892aec08da0ae7b18c35c2be0395e37e4
Danny van Kooten <dannyvankooten@gmail.com>
2016-01-09 23:09:05 +0700

    Minor clean-up

--- icp.py
+++ icp.py
@@ -1,12 +1,6 @@
-import sys
 import requests
 import xml.etree.ElementTree as ET
-import operator
-import math
-import os
-import argparse
-
-# TODO: Allow specifying a filter file as argument
+import operator, math, os, argparse
 
 # Read CLI arguments
 parser = argparse.ArgumentParser(description='Genereert formulier HTML voor de Belastingdienst opgave site.')
@@ -21,21 +15,17 @@ moneybird_password = os.environ.get('MONEYBIRD_PASSWORD')
 
 # Define Company class
 class Company:
-
 	total = 0.00
 	vat_number = ""
 
 	def __init__(self, vat_number):
 		self.vat_number = vat_number
 
-	def __repr__(self):
-		return self.vat_number + ": " + str( self.total) + "\n"
-
 # Read filter.xml into variable
 payload = open('filters/icp.xml', 'r').read().replace('{{tax_rate_id}}', str(args.tax_rate_id)).replace('{{period}}',args.period)
 
 # Request MoneyBird API
-r = requests.post('https://'+moneybird_username+'.moneybird.nl/api/v1.0/invoices/filter/advanced.xml',
+response = requests.post('https://'+moneybird_username+'.moneybird.nl/api/v1.0/invoices/filter/advanced.xml',
 	auth=(moneybird_email, moneybird_password),
 	data=payload,
 	headers={
@@ -43,12 +33,12 @@ r = requests.post('https://'+moneybird_username+'.moneybird.nl/api/v1.0/invoices
 	'Accept': 'application/xml'
 	}
 )
-response = r.content
 
 # Turn response into XML list
-root = ET.fromstring(response)
+root = ET.fromstring(response.content)
 companies = {}
 
+# Calculate total for each company
 for invoice in root:
 	tax_number = invoice.find('tax-number').text
 	total = float(invoice.find('total-price-incl-tax-base').text)
@@ -68,5 +58,6 @@ template = open('templates/icp.html', 'r').read()
 for index, company in enumerate(companies):
 	html = html + template.replace("{{index}}", str(index)).replace('{{vat_number}}', company.vat_number[2:len(company.vat_number)]).replace('{{total_services}}', str(math.floor(company.total))).replace('{{total_goods}}', '').replace('{{country}}', company.vat_number[0:2])
 
+# Write to file and open
 file = open('/tmp/form.html','w').write(html)
 os.system('open /tmp/form.html -a "Sublime Text"')
