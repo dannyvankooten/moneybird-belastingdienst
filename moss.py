dc4900731c099c82d00a16561f0dd69aebbcdf37
Danny van Kooten <dannyvankooten@gmail.com>
2016-01-09 23:08:48 +0700

    First draft of MOSS code flow.

---
+++ moss.py
@@ -0,0 +1,14 @@
+# Get env vars
+
+# Query tax rates
+
+# For each tax rate:
+
+	# Query all invoices with this tax rate in the specified period
+
+	# Calculate total excl. VAT + total VAT
+
+	# Build form row HTML from template
+	# country, vat_rate, vat_total, total_excl_vat
+
+# Write to file & open
